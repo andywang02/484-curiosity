3f6691066d022d41769d9faedf3b1abe0c6536ab
Andy Wang <wangandy@mit.edu>
2022-05-06 00:21:41 -0400

    d

--- rlpyt/envs/pycolab/pycolab/examples/better_scrolly_maze.py
+++ rlpyt/envs/pycolab/pycolab/examples/better_scrolly_maze.py
@@ -261,7 +261,7 @@ def make_game(level):
   art = ['#' * 15 + i + "#" * 15 for i in art]
   art = ['#' * len(art[0])] * 5 + art + ['#' * len(art[0])] * 5
   return ascii_art.ascii_art_to_game(
-      MAZES_ART[level], what_lies_beneath=' ',
+      art, what_lies_beneath=' ',
       sprites={
           'P': PlayerSprite,
           'a': PatrollerSprite,
